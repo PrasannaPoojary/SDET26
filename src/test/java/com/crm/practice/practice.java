b9b72378852a405408dfdacab5914f1a1664c3a2
Prasanna <Prasanna@DESKTOP-VIB6UCD>
2021-12-09 15:02:53 +0530

    test and demo added

---
+++ src/test/java/com/crm/practice/practice.java
@@ -0,0 +1,14 @@
+package com.crm.practice;
+
+import org.testng.annotations.Test;
+
+public class practice {
+	
+	@Test
+	public void demo() {
+		System.out.println("execute user 1");
+		
+	}
+	
+
+}
