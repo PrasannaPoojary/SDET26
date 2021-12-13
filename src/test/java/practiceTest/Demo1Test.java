fcc4e564faaadf8cef2f77b8d030d69bc1a77a41
Prasanna <Prasanna@DESKTOP-VIB6UCD>
2021-12-13 17:59:15 +0530

    kkkkkkkv aa

--- src/test/java/practiceTest/Demo1Test.java
+++ src/test/java/practiceTest/Demo1Test.java
@@ -6,7 +6,7 @@ public class Demo1Test {
 	
 	@Test
 	public   void demo() {
-		 System.out.println("quwuijwij6");
+		 System.out.println("quwuijwij6kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
 		 System.out.println("quwuijwij6");
 		 System.out.println("quwuijwij6");
 		
